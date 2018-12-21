72e471eae6e92ef1d5fd00b564fb5b2b14f18c4e
Mario Fernando Diaz Olivera <mariidiaz78@gmail.com>
2018-12-21 10:06:35 -0600

    cambios WS

--- WBSupportCenter/WSSupportCenter/App_Code/WSsupportCenterClass.cs
+++ WBSupportCenter/WSSupportCenter/App_Code/WSsupportCenterClass.cs
@@ -4,8 +4,7 @@ using System.Web.Services;
 using System.Data;
 using SupportCenter.Negocio;
 using SupportCenter.Entidades;
-
-
+using System.Web.Script.Services;
 
 [System.Web.Script.Services.ScriptService]
 
@@ -14,16 +13,24 @@ public class WSsupportCenterClass : System.Web.Services.WebService {
     readonly NGBusqueda Busqueda = new NGBusqueda();
 
     [WebMethod]
-    public DataTable WSOptenerArt() {
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public List<Articulos> WSOptenerArt() {
         return Busqueda.NG_ObtenerArt();
     }
 
     [WebMethod]
-    public DataTable WSOptenerCatg() {
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public List<CategoriasxSubcategorias> WSOptenerCatg() {
 
         return Busqueda.NG_ObtenerCatg();
     }
 
+    [WebMethod(EnableSession = true)]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public List<BusquedaTitulo> WSBusquedaTitulo(string palabra) {
+
+        return Busqueda.NG_BusquedaTitulo(palabra);
+    }
 
 }
 
