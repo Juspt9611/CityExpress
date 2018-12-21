1272e3d29a2120aded4f7d76dfe61ee9dc67d869
Mario Fernando Diaz Olivera <mariidiaz78@gmail.com>
2018-12-21 09:55:21 -0600

    Archivos nuevos

---
+++ WBSupportCenter/SupportCenter.Entidades/BusquedaTitulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportCenter.Entidades {
+    public class BusquedaTitulo {
+        public int idarticulo { get; set; }
+        public string nombreArticulo { get; set; }
+    }
+}
