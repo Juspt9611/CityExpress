9f5c52e5d43c4aa1ae6a283979d40a66590fdc71
Juspt9611 <ju_9611spt@outlook.com>
2018-12-21 09:56:12 -0600

    Archivos nuevos

---
+++ WBSupportCenter/SupportCenter.Datos/DTArticulo.cs
@@ -0,0 +1,81 @@
+﻿using SupportCenter.Framework.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportCenter.Datos
+{
+    public class DTArticulo
+    {
+        public int DT_RegistrarArticulo(string nombreArticulo, string contenido, int[] categorias)
+        {
+            int error = 0;
+            SqlConnection connection = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                using (connection = Conexion.ObtieneConexion("ConexionBD"))
+                {
+
+                    SqlDataReader consulta;
+                    connection.Open();
+
+                    var parametros = new[]
+                    {
+                        ParametroAcceso.CrearParametro("@nombreArticulo", SqlDbType.VarChar, nombreArticulo , ParameterDirection.Input),
+                        ParametroAcceso.CrearParametro("@contenido", SqlDbType.VarChar, contenido , ParameterDirection.Input)
+                    };
+
+
+                    consulta = Ejecuta.ProcedimientoAlmacenado(connection, "SP_InsertarArticulos", parametros);
+                    dt.Load(consulta);
+                    connection.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                error = 1;
+                Console.WriteLine(ex);
+            }
+
+            return error;
+        }
+
+
+        public DataTable DT_ConsultarArxtxValidar()
+        {
+
+            SqlConnection connection = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                using (connection = Conexion.ObtieneConexion("ConexionBD"))
+                {
+
+                    SqlDataReader consulta;
+                    connection.Open();
+
+                    consulta = Ejecuta.ProcedimientoAlmacenado(connection, "SP_ConsultaArticulosPendientesxValidar");
+                    dt.Load(consulta);
+                    connection.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+            return dt;
+        }
+
+    }
+}
---
+++ WBSupportCenter/SupportCenter.Negocio/NGArticulos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportCenter.Datos;
+using System.Data;
+
+namespace SupportCenter.Negocio
+{
+    public class NGArticulos
+    {
+        public int NG_RegistrarArt(string nombreArticulo, string contenido, int[] categorias)
+        {
+            DTArticulo Objeto = new DTArticulo();
+            return Objeto.DT_RegistrarArticulo(nombreArticulo, contenido, categorias);
+        }
+
+        //public DataSet NG_ConsultarArtxValidar()
+        //{
+        //    DTArticulo Objeto = new DTArticulo();
+        //    return Objeto.DT_ConsultarArxtxValidar();
+        //}
+    }
+}
